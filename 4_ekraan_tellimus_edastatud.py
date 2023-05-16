529f16d8ad394e17b9e47bf6012fedd5d732384d
gregorkokk <kokkgregor@gmail.com>
2023-05-16 12:15:15 +0300

    4 ekraan ehk tellimus edastatud done

--- 4_ekraan_tellimus_edastatud.py
+++ 4_ekraan_tellimus_edastatud.py
@@ -1,4 +1,4 @@
-import PySimpleGUI as sq
+import PySimpleGUI as sg
 
 # Set the theme for the screen
 sg.theme("Green")
@@ -8,17 +8,12 @@ font = "Times New Roman"
 
 # Define the layout
 layout = [
-    [sg.VerticalSeparator(pad=(0, 30))],
-    [sg.Text("Kaks Paskalit", font=(font, 48), justification="center", pad=((55, 0), (0, 0)))],
-    [sg.VerticalSeparator(pad=(0, 10))],
-    [sg.Image("logoo.png", size=(200, 200), pad=((95, 0), (0, 0)))],
-    [sg.VerticalSeparator(pad=(0, 20))],
-    [sg.VerticalSeparator(pad=(0, 50))],
-    [sg.Button("Menüü", size=(14, 1), font=(font, 22), button_color=("black", "green"), pad=((106, 0), (0, 0)))]
+    [sg.Image("order_delivered.png")]
+
 ]
 
 # Create the window
-window = sg.Window("Restaurant Kaks Paskalit", layout, size=(400, 600))
+window = sg.Window("Restaurant Kaks Paskalit", layout, size=(400, 700))
 
 # Event loop
 while True:
